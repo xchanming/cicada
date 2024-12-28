82eead187eb9b177c6441fdadef717fed55b2308
gxs <lyricsong47@gmail.com>
2024-12-28 10:33:00 +0800

    fix: media folder creation not possible with enter key

--- tests/migration/Core/V6_6/Migration1663402950SetDoubleOptinCustomerActiveTest.php
+++ tests/migration/Core/V6_6/Migration1663402950SetDoubleOptinCustomerActiveTest.php
@@ -3,7 +3,6 @@
 namespace Cicada\Tests\Migration\Core\V6_6;
 
 use Cicada\Core\Defaults;
-use Cicada\Core\Framework\Feature;
 use Cicada\Core\Framework\Test\TestCaseBase\KernelLifecycleManager;
 use Cicada\Core\Framework\Uuid\Uuid;
 use Cicada\Core\Migration\V6_6\Migration1663402950SetDoubleOptinCustomerActive;
@@ -69,10 +68,6 @@ class Migration1663402950SetDoubleOptinCustomerActiveTest extends TestCase
             'guest' => 0,
             'created_at' => $now->format('Y-m-d H:i:s'),
         ];
-
-        if (!Feature::isActive('v6.7.0.0')) {
-            $data['default_payment_method_id'] = $this->connection->fetchOne('SELECT id FROM `payment_method` WHERE `active` = 1');
-        }
 
         return $this->connection->insert('customer', $data);
     }
