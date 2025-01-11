fa1dff4fa07bd0c1ab2652e55a7dcac3d5893ad1
gxs <lyricsong47@gmail.com>
2025-01-11 10:35:18 +0800

    chore(ci): fix codecov reports

--- tests/unit/Core/Framework/Api/Command/DumpSchemaCommandTest.php
+++ tests/unit/Core/Framework/Api/Command/DumpSchemaCommandTest.php
@@ -2,12 +2,12 @@
 
 namespace Cicada\Tests\Unit\Core\Framework\Api\Command;
 
-use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\TestCase;
 use Cicada\Core\Framework\Api\ApiDefinition\DefinitionService;
 use Cicada\Core\Framework\Api\ApiDefinition\Generator\CachedEntitySchemaGenerator;
 use Cicada\Core\Framework\Api\ApiDefinition\Generator\EntitySchemaGenerator;
 use Cicada\Core\Framework\Api\Command\DumpSchemaCommand;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Tester\CommandTester;
 use Symfony\Contracts\Cache\CacheInterface;
 
